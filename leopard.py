bfd003b2d35d135de90b42f8ed0eea6b91d5c9ca
MoDesigns1 <mohabthe@gmail.com>
2022-11-29 10:32:38 +0000

    edit

--- leopard.py
+++ leopard.py
@@ -8,32 +8,41 @@ import csv
 class Leopard:
     def __init__(self, filepath: str) -> None:
         self.filepath = filepath
-        self.header = self.get_header()
-        self.data = self.get_data()
+        global empty
+        empty = False
+        # Attempt to open the file and check its length.
+        try:
+            testF = open(filepath, 'r')
+            val = testF.read()
+            testF.close()
+            if len(val) == 0:
+                empty = True
+                print("File is empty.")
+                self.header = None
+                self.data = None
+            else:
+                with open(filepath, 'r') as fp:
+                    reader = csv.reader(fp)
+                    self.header = next(reader)  # Gets the header
+                    self.data = [row for row in reader]  # Creates a list
+        except FileNotFoundError:
+            print("File not found.")
+            empty = True
+            self.header = None
+            self.data = None
 
     def get_header(self) -> list:
-        # get headers from the csv file
-        try:
-            with open(self.filepath, 'r') as fp:
-                reader = csv.reader(fp)
-                header = next(reader)  # Gets the header
-            return header
-        except StopIteration:  # If the file is empty it returns None
-            return ("File is empty.")
-        except FileNotFoundError:  # If the file is not found it returns None
-            return ("File not found.")
+        # checks if file is empty
+        if empty:
+            return None
+        else:
+            return self.header
 
     def get_data(self) -> list:
-        try:
-            with open(self.filepath, 'r') as fp:
-                reader = csv.reader(fp)
-                next(reader)  # Skips the header
-                data = [row for row in reader]  # Creates a list of lists
-            return data
-        except StopIteration:
+        if empty:
             return None
-        except FileNotFoundError:
-            return None
+        else:
+            return self.data
 
     def stats(self) -> dict:
         if self.header is None:  # Checks if file is empty
@@ -51,15 +60,12 @@ class Leopard:
             i = self.header.index(col)
             try:
                 runningMax = float(self.data[i][j])
-            except ValueError:
-                runningMax = 0
-            except ValueError:
+            except (ValueError, IndexError):
                 runningMax = 0
             try:
                 runningMin = float(self.data[i][j])
-            except IndexError:
-                pass
-            except ValueError:  # If the value is not an integer it passes
+            except (IndexError, ValueError):
+                runningMin = 100000
                 pass
             for i in range(0, len(self.data)):  # Loops through the rows
                 try:
@@ -90,55 +96,82 @@ class Leopard:
             return None
         if self.data is None:  # Checks if file is empty
             return None
-        with open(filepath, 'w') as fp:  # Creates a new file
-            # Below is the html code
-            fp.write("<html>\n")
-            fp.write("<head>\n")
-            fp.write("<title>Stats</title>\n")
-            fp.write("</head>\n")
-            fp.write("<body>\n")
-            fp.write("<table>\n")
-            fp.write("<style>\n")
-            # Below is CSS styling to make the table look nicer.
-            fp.write("td:nth-child(even), th:nth-child(even)")
-            fp.write("{ background-color: #D6EEEE; }\n")
-            fp.write("td:nth-child(odd), th:nth-child(odd)")
-            fp.write("{background-color: #F0F8FF; }\n")
-            fp.write("table, th, td { border: 1px solid black;")
-            fp.write("text-align: center; \n")
-            fp.write("border-collapse: collapse; }\n")
-            fp.write("table { width: 100%; }\n")
-            fp.write("</style>\n")
-            fp.write("<tr>\n")
-            fp.write("<th>\n")
-            fp.write("<th>Count</td>\n")
-            fp.write("<th>Mean</td>\n")
-            fp.write("<th>Max</td>\n")
-            fp.write("<th>Min</td>\n")
-            fp.write("</th>\n")
-            fp.write("</tr>\n")
-            for column in stats:  # Loops through the columns
-                fp.write("<tr>\n")
-                # Writes column names.
-                fp.write(f"<th>{column.capitalize()}</th>\n")
-                for key in stats[column]:
-                    # Writes the key and value
-                    fp.write("<td>")
-                    fp.write(f"{(stats[column][key])}</td>\n")
-                fp.write("</tr>\n")
-            fp.write("</table>\n")
-            fp.write("</body>\n")
-            fp.write("</html>\n")
+        # Initalizes the html variable that stores the code.
+        html = """<html>
+        <head>
+        <title>Stats</title>
+        </head>
+        <body>
+        <table>
+        <style>
+        td:nth-child(even), th:nth-child(even) {
+        background-color: #45C4B0;
+        }
+        td:nth-child(odd), th:nth-child(odd) {
+        background-color: #13678A;
+        }
+        table, th, td {
+        border: 1px solid white;
+        text-align: center;
+        border-collapse: collapse;
+        }
+        table {
+        width: 50%;
+        }
+        th, td {
+        color: white;
+        }
+        table {
+        padding: 10px;
+        margin-left: auto;
+        margin-right: auto;
+        }
+        h1 {
+        text-align: center;
+        }
+        * {
+        font-family: Arial, Helvetica, sans-serif;
+        }
+        table {
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+        }
+        th {
+        border: 1px solid white;
+        text-align: center;
+        border-collapse: collapse;
+        }
+        </style>
+        <h1>Stats of the CSV file</h1>
+        <tr>
+        <th>Stats</th>
+        <th>Count</th>
+        <th>Mean</th>
+        <th>Max</th>
+        <th>Min</th>
+        </tr>
+        """
+        for column in stats:
+            html += f"<tr><th>{column.capitalize()}</th>"
+            for key in stats[column]:
+                html += f"<td>{stats[column][key]}</td>"
+            html += "</tr>"
+        html += "</table></body></html>"
+        with open(filepath, 'w') as fp:
+            fp.write(html)
+            fp.close()
 
-    def count_instances(self, c1Name: str = "None", c1Val: int = 0,
+    def count_instances(self, c1Name: str = "None", c1Val: str = "None",
                         c2Name: str = "None", c2Val: str = "None",
                         c3Name: str = "None", c3Val: str = "None") -> int:
         """
     To count the number of instances in the data that satisfy the conditions,
     the user must enter upto 3 critera followed by the value of the criteria.
     For example:
-    objectName.count_instances("column1", intVal (i.e 1), "column2",
+    objectName.count_instances("column1", "value1", "column2",
                                "value2", "column3", "value3")
+    The values entered could be integers or strings.
+    Integers have to be passed by as a string, for example:
+    objectName.count_instances("column1", "1", "column2", "2")
     If less than 3 criteria are submitted, the empty ones are printed as None.
     Returned is the count of each instance that meets the criteria in the data.
     If a criteria is given that doesnt exist it returns an error message.
@@ -147,15 +180,17 @@ class Leopard:
             return None
         if self.data is None:
             return None
-        count1 = 0
-        count2 = 0
-        count3 = 0
+        self.header = [head.lower() for head in self.header]
+        count = [0, 0, 0]
+        criteria = [c1Name, c2Name, c3Name]
         countList = []
+        criteria = [crit.lower() for crit in criteria]
+        if "None" in criteria:
+            criteria.remove("None")
         # Attempts to access the index of criterias
         try:
-            self.header.index(c1Name)
-            self.header.index(c2Name)
-            self.header.index(c3Name)
+            for i in criteria:
+                self.header.index(i)
         # If the criteria is not present it returns a error.
         except IndexError:
             countList.append("Error: One or more of criteria not found.")
@@ -167,22 +202,28 @@ class Leopard:
             for i in range(0, len(self.data)):  # Loops through the rows
                 # Checks if the value is equal to the criteria.
                 # adds to count if it matches.
-                if self.data[i][self.header.index(c1Name)] == str(c1Val):
-                    count1 += 1
-                if self.data[i][self.header.index(c2Name)] == str(c2Val):
-                    count2 += 1
-                if self.data[i][self.header.index(c3Name)] == str(c3Val):
-                    count3 += 1
+                if c1Name != "None":
+                    if self.data[i][self.header.index(criteria[0]
+                                                      )] == str(c1Val):
+                        count[0] += 1
+                if c2Name != "None":
+                    if self.data[i][self.header.index(criteria[1]
+                                                      )] == str(c2Val):
+                        count[1] += 1
+                if c3Name != "None":
+                    if self.data[i][self.header.index(criteria[2]
+                                                      )] == str(c3Val):
+                        count[2] += 1
         except IndexError:
             pass
         except ValueError:  # If the value is not an integer it passes
             pass
         # Below adds the counts with names & values to an array to be returned.
-        countList.extend([f"the amount of instances of '{c1Name}"
-                          f" = {c1Val}' is {count1}", "the amount of"
-                          f" instances of '{c2Name} = {c2Val}' is"
-                          f" {count2}", "the amount of instances of"
-                          f" '{c3Name} = {c3Val}' is {count3}"])
+        countList.extend([f"the amount of instances of '{c1Name.capitalize()}"
+                          f" = {c1Val}' is {count[0]}", "the amount of"
+                          f" instances of '{c2Name.capitalize()} = {c2Val}' is"
+                          f" {count[1]}", "the amount of instances of"
+                          f" '{c3Name.capitalize()} = {c3Val}' is {count[2]}"])
         # Returns as a string
         return (f"\n{countList[0]} \n{countList[1]} \n{countList[2]}")
 
@@ -194,7 +235,7 @@ if __name__ == "__main__":
     # test diabetes_data.csv
     # test = Leopard("diabetes_data.csv")
     # print(test.get_header())
-    # (test.get_data())
+    # print(test.get_data())
     # stats = test.stats()
     # print(stats)
     # test.html_stats(stats, "diabetes.html")
@@ -207,6 +248,7 @@ if __name__ == "__main__":
     # stats2 = test2.stats()
     # print(stats2)
     # test2.html_stats(stats2, "fide2021.html")
+    # print(test2.count_instances("Rank", 18, "Country", "RUS", "Games", "0"))
     # print()
 
     # test student.csv
@@ -216,5 +258,5 @@ if __name__ == "__main__":
     # stats3 = test3.stats()
     # print(stats3)
     # test3.html_stats(stats3, "student.html")
-    # print(test3.count_instances("studytime", 3, "sex", "F", "age", 18))
-    print("TEST")
+    # print(test3.count_instances("STUDYTIME", "3", "sex", "F", "age", "18"))
+    print()
